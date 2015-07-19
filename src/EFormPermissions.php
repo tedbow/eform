1ce03279ef97b8a1ad9057c23cfad74bd977b43e
Ted Bowman <ted.git@tedbow.com>
2015-07-19 17:36:04 -0400

    added dynamic permissions

---
+++ src/EFormPermissions.php
@@ -0,0 +1,83 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\eform\EFormPermissions.
+ */
+
+namespace Drupal\eform;
+
+use Drupal\Core\Routing\UrlGeneratorTrait;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\eform\Entity\EFormType;
+
+/**
+ * Defines a class containing permission callbacks.
+ */
+class EFormPermissions {
+
+  use StringTranslationTrait;
+  use UrlGeneratorTrait;
+
+  /**
+   * Gets an array of eform type permissions.
+   *
+   * @return array
+   *   The eform type permissions.
+   *   @see \Drupal\user\PermissionHandlerInterface::getPermissions()
+   */
+  public function eformTypePermissions() {
+    $perms = array();
+    // Generate eform permissions for all eform types.
+    foreach (EFormType::loadMultiple() as $type) {
+      $perms += $this->buildPermissions($type);
+    }
+
+    return $perms;
+  }
+
+  /**
+   * Builds a standard list of eform permissions for a given type.
+   *
+   * @param \Drupal\eform\Entity\EFormType $type
+   *   The machine name of the eform type.
+   *
+   * @return array
+   *   An array of permission names and descriptions.
+   */
+  protected function buildPermissions(EFormType $type) {
+    $type_id = $type->id();
+    $type_params = array('%type_name' => $type->label());
+
+    return array(
+      "submit $type_id eform" => array(
+        'title' => $this->t('%type_name: Submit', $type_params),
+      ),
+      "edit own $type_id submissions" => array(
+        'title' => $this->t('%type_name: Edit own submissions', $type_params),
+      ),
+      /*"edit any $type_id submissions" => array(
+        'title' => $this->t('%type_name: Edit any submissions', $type_params),
+      ),*/
+      "delete own $type_id submissions" => array(
+        'title' => $this->t('%type_name: Delete own submissions', $type_params),
+      ),
+      // Which other permissions should be supported?
+      /*"delete any $type_id submissions" => array(
+        'title' => $this->t('%type_name: Delete any submissions', $type_params),
+      ),
+      "view $type_id revisions" => array(
+        'title' => $this->t('%type_name: View revisions', $type_params),
+      ),
+      "revert $type_id revisions" => array(
+        'title' => $this->t('%type_name: Revert revisions', $type_params),
+        'description' => t('Role requires permission <em>view revisions</em> and <em>edit rights</em> for eforms in question, or <em>administer eforms</em>.'),
+      ),
+      "delete $type_id revisions" => array(
+        'title' => $this->t('%type_name: Delete revisions', $type_params),
+        'description' => $this->t('Role requires permission to <em>view revisions</em> and <em>delete rights</em> for eforms in question, or <em>administer eforms</em>.'),
+      ),*/
+    );
+  }
+
+}
